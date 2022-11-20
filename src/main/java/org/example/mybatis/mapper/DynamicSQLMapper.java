f32dbc2d1ebd11adc507ddc8ee423e081e5969c6
robert <robert.ljx.de.simon@gmail.com>
2022-11-20 20:58:50 +1100

    dynamic mapper

--- src/main/java/org/example/mybatis/mapper/DynamicSQLMapper.java
+++ src/main/java/org/example/mybatis/mapper/DynamicSQLMapper.java
@@ -1,9 +1,14 @@
 package org.example.mybatis.mapper;
 
+import org.apache.ibatis.annotations.Param;
 import org.example.mybatis.pojo.Emp;
 
 import java.util.List;
 
 public interface DynamicSQLMapper {
     List<Emp> getEmpByConditions(Emp emp);
+    List<Emp> getEmpByChoose(Emp emp);
+    void insertMoreEmp(@Param("emps") List<Emp> emps);
+    void deleteMoreEmp(@Param("empIds")Integer[] empIds);
+
 }
--- src/test/java/org/example/mybatis/test/DynamicSQLMapperTest.java
+++ src/test/java/org/example/mybatis/test/DynamicSQLMapperTest.java
@@ -6,6 +6,7 @@ import org.example.mybatis.pojo.Emp;
 import org.example.mybatis.utils.SqlSessionUtil;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class DynamicSQLMapperTest {
@@ -16,5 +17,22 @@ public class DynamicSQLMapperTest {
         Emp emp=new Emp(null,"admin",23,"male");
         List<Emp> empByConditions = mapper.getEmpByConditions(emp);
         empByConditions.forEach(System.out::println);
+    }
+    @Test
+    public void testInsertMultiEmp(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        DynamicSQLMapper mapper = sqlSession.getMapper(DynamicSQLMapper.class);
+        Emp emp=new Emp(null,"admin",23,"male");
+        Emp emp2=new Emp(null,"admin2",23,"male");
+        Emp emp3=new Emp(null,"admin3",23,"male");
+        List<Emp> emps = Arrays.asList(emp, emp2, emp3);
+        mapper.insertMoreEmp(emps);
+    }
+    @Test
+    public void testDeleteMultiEmp(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        DynamicSQLMapper mapper = sqlSession.getMapper(DynamicSQLMapper.class);
+        Integer[]  empIds=new Integer[]{6,7};
+        mapper.deleteMoreEmp(empIds);
     }
 }
