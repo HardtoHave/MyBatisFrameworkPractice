63b072f38056a61e3048adee82a66bfa8a8f1269
robert <robert.ljx.de.simon@gmail.com>
2022-11-23 17:36:40 +1100

    page test

--- mybatis_generator/src/test/java/org/example/mybatis/test/PageTest.java
+++ mybatis_generator/src/test/java/org/example/mybatis/test/PageTest.java
@@ -1,6 +1,8 @@
 package org.example.mybatis.test;
 
+import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.apache.ibatis.session.SqlSession;
 import org.example.mybatis.mapper.EmpMapper;
 import org.example.mybatis.pojo.Emp;
@@ -15,8 +17,11 @@ public class PageTest {
     public void testPage(){
         SqlSession sqlSession = SqlSessionUtil.getSqlSession();
         EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
-        PageHelper.startPage(1,1);
+        Page<Object> page = PageHelper.startPage(1, 4);
         List<Emp> emps = mapper.selectByExample(null);
         emps.forEach(System.out::println );
+        System.out.println(page);
+        PageInfo<Emp> empPageInfo = new PageInfo<>(emps, 5);
+        System.out.println(empPageInfo);
     }
 }
